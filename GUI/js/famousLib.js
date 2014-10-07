e3cc6c0e4aa4091f2cbeadcb644bcce846c70f02
Schreda <strieder.georg@gmail.com>
2014-10-08 00:27:02 +0200

    main screen added issue does still exist regarding resizing work in progress

--- GUI/js/famousLib.js
+++ GUI/js/famousLib.js
@@ -7,7 +7,9 @@ define(function(require, exports, module) {
         this.Transform = require('famous/core/Transform');
         this.ImageSurface = require('famous/surfaces/ImageSurface');
         this.ContainerSurface = require('famous/surfaces/ContainerSurface');
+        this.Surface = require('famous/core/Surface');
         this.StateModifier = require('famous/modifiers/StateModifier');
+        this.Transform = require('famous/core/Transform');
         //APP MODULES
         this.Header = require('Header');
         this.Footer = require('Footer');
@@ -15,16 +17,26 @@ define(function(require, exports, module) {
 
     FamousLib.prototype.loadGlobalVars = function(){
         this.headerLogo = "./css/headerLogo.svg";
+        this.settingsIcon = "./css/settings.png";
+        this.trophyIcon = "./css/trophy.png";
+        this.shareIcon = "./css/share.png";
+
         this.winSize = {width: window.innerWidth, height: window.innerHeight};
         this.headerSize = {width: this.winSize.width, height: parseInt(this.winSize.height* 0.107)};
         this.logoSize = {width: undefined, height: parseInt(this.headerSize.height *0.8)};
         this.logoPosition = {x: parseInt( (this.headerSize.width - this.logoSize.width)*0.5 ), y: 0 };
+        this.buttonSize = {width: parseInt( this.headerSize.width*0.325) , height: parseInt( this.headerSize.height*0.5)};
+        this.buttonFontSize = parseInt(this.buttonSize.height * 0.482) + "px";
     }
 
     FamousLib.prototype.loadGlobalModifiers = function(){
         this.centerXModifier = new this.StateModifier({
             align: [0.5, 0.5],
-            origin: [0.5, 0.5],
+            origin: [0.5, 0.5]
+        });
+        this.centerYModifier = new this.StateModifier({
+            align: [0, 0.5],
+            origin: [0, 0.5]
         });
         this.bottomModifier = new this.StateModifier({
             align:[0, 1],
--- GUI/js/footer.js
+++ GUI/js/footer.js
@@ -11,13 +11,72 @@ define(function(require, exports, module) {
         var self = this;
         var LIB = self.LIB;
 
-        var footerContainer = new LIB.ContainerSurface({
+        self.footerContainer = new LIB.ContainerSurface({
             size: [LIB.headerSize.width, LIB.headerSize.height],
             classes: ['appFooter']
         });
- 
-        self.context.add(LIB.bottomModifier).add(footerContainer);
+        //ADD NEW GAME BUTTON
+        self.newGameButton = new LIB.Surface({
+            size: [ 180 , LIB.buttonSize.height],
+            content: "New game",
+            classes: ['button', 'newGame'],
+            properties: {
+                fontSize: LIB.buttonFontSize
+            }
+        });
+        self.newGameModifier = new LIB.StateModifier({
+            origin: [0, 0.5],
+            align: [0.05, 0.5]
+        });
+        self.footerContainer.add(self.newGameModifier).add(self.newGameButton);
 
+        //BUTTONS SETTUP
+        var width = LIB.winSize.width*0.95;
+        var iconSize = parseInt(LIB.buttonSize.height);
+        var spaceBTWElemnets = 20;
+        var offset = 0;
+        
+        offset = width;
+        self.settingsButton = new LIB.ImageSurface({
+            size: [iconSize, iconSize],
+            content: LIB.settingsIcon,
+            classes: ['iconButton']
+        });
+        self.settingsModifier = new LIB.StateModifier({
+            origin: [1, 0.5],
+            align: [0, 0.5],
+            transform: LIB.Transform.translate(offset, 0, 0)
+        });
+        self.footerContainer.add(self.settingsModifier).add(self.settingsButton);
+
+        offset -= (iconSize + spaceBTWElemnets);
+        self.trophyButton = new LIB.ImageSurface({
+            size: [iconSize, iconSize],
+            content: LIB.trophyIcon,
+            classes: ['iconButton']
+        });
+        self.trophyModifier = new LIB.StateModifier({
+            origin: [1, 0.5],
+            align: [0, 0.5],
+            transform: LIB.Transform.translate(offset , 0, 0)
+        });
+        self.footerContainer.add(self.trophyModifier).add(self.trophyButton);
+
+        offset -= (iconSize + spaceBTWElemnets);
+        self.shareButton = new LIB.ImageSurface({
+            size: [iconSize, iconSize],
+            content: LIB.shareIcon,
+            classes: ['iconButton']
+        });
+        self.shareModifier = new LIB.StateModifier({
+            origin: [1, 0.5],
+            align: [0, 0.5],
+            transform: LIB.Transform.translate(offset, 0, 0)
+        });
+        self.footerContainer.add(self.shareModifier).add(self.shareButton);
+        //ADD FOOTER TO CONTEXT
+        self.context.add(LIB.bottomModifier).add(self.footerContainer);
+    
     }
 
     return Footer;
