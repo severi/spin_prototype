7cbf42fba2c44a86efd4869d412cb4c59b63fbb2
Schreda <strieder.georg@gmail.com>
2014-10-30 18:37:44 +0100

    basic cube color changed

--- Prototyp/js/settings.js
+++ Prototyp/js/settings.js
@@ -10,14 +10,14 @@ var settings = {
 	initPreTime: 30,
 	initScore: 0,
 	stage: 2,
-	defaultColor : 0x272822
+	defaultColor : 0xececec
 };
 
 var faceColors = {
 		red: 0xf64747,
 		yellow: 0xf9bf3b,
 		green: 0x03c9a9,
-		blue: 0x19b5fe,
+		blue: 0x4183D7,
 };
 
 
